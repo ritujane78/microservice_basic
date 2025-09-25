8195ce7a6ee7b6ede4643562630b6ef669953a0b
Ritu Bafna <ritujane78@gmail.com>
2025-09-25 12:30:54 +0545

    add @EnableMthodSecurity annotation to make @Secured, @preAuthorize , @PostAuthorize annotations work

--- src/main/java/com/jane/ws/api/ResourceServer/controllers/UsersController.java
+++ src/main/java/com/jane/ws/api/ResourceServer/controllers/UsersController.java
@@ -1,8 +1,12 @@
 package com.jane.ws.api.ResourceServer.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jane.ws.api.ResourceServer.responses.UserRest;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -11,5 +15,20 @@ public class UsersController {
     @GetMapping("/status/check")
     public String status(){
         return "Working...";
+    }
+
+    @PreAuthorize("hasRole('developer') or #id == #jwt.subject" )
+//    @PreAuthorize("hasAuthority('ROLE_developer')")
+    @Secured("ROLE_developer")
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt){
+        return "User deleted with id: " + id + " and jwt subject: " + jwt.getSubject();
+    }
+    @PostAuthorize("returnObject.id == #jwt.subject" )
+//    @PreAuthorize("hasAuthority('ROLE_developer')")
+    @Secured("ROLE_developer")
+    @GetMapping("/{id}")
+    public UserRest getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt){
+        return new UserRest("8af74e98-5467-459c-9a63-1888e7be1785","Ritu", "Bafna");
     }
 }
--- src/main/java/com/jane/ws/api/ResourceServer/security/SecurityConfig.java
+++ src/main/java/com/jane/ws/api/ResourceServer/security/SecurityConfig.java
@@ -3,11 +3,15 @@ package com.jane.ws.api.ResourceServer.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig {
 
     @Bean
